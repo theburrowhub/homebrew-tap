3799d180d5d70125d9f4bca08bb17807f8f72303
Sergio Tejón <sergio.tejon@gmail.com>
2026-01-07 15:56:42 +0100

    chore(cherry-go): update to version 0.4.0 and standardize binary URLs

--- Formula/cherry-go.rb
+++ Formula/cherry-go.rb
@@ -4,51 +4,36 @@
 class CherryGo < Formula
   desc "A CLI tool for partial file versioning from other Git repositories"
   homepage "https://github.com/theburrowhub/cherry-go"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go-macos-arm64.tar.gz"
-      sha256 "3b0b0d9a7dc6ce1c1a09ec4672db49003d5f035cb8e5c2f045d9923be1462587"
-
-      def install
-        bin.install "cherry-go-macos-arm64" => "cherry-go"
-        generate_completions_from_executable(bin/"cherry-go", "completion")
-      end
+      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go_#{version}_darwin_arm64.tar.gz"
+      sha256 "b44dc7d8f5e8ece5d507ecce82289c32667c92a93bacbf5125efd09b93e09515"
     end
 
     on_intel do
-      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go-macos-amd64.tar.gz"
-      sha256 "75a93b7aa22784214a2f9ddae68f26f672a02f4dea8c87e7bea2ffdd614c2d6c"
-
-      def install
-        bin.install "cherry-go-macos-amd64" => "cherry-go"
-        generate_completions_from_executable(bin/"cherry-go", "completion")
-      end
+      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go_#{version}_darwin_amd64.tar.gz"
+      sha256 "04e3e283968f338efc11145887dc6d0d435c83cb345c7cf5444441698ee123ac"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go-linux-arm64.tar.gz"
-      sha256 "483f0608f5716af2fd4b9d1957b0449fdc1409a8111667b00969ebea4229834a"
-
-      def install
-        bin.install "cherry-go-linux-arm64" => "cherry-go"
-        generate_completions_from_executable(bin/"cherry-go", "completion")
-      end
+      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go_#{version}_linux_arm64.tar.gz"
+      sha256 "cbc4a86e0a8b2b37fce1cfb2f0f6062f92016b0b162bf17d67f38c547898b9db"
     end
 
     on_intel do
-      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go-linux-amd64.tar.gz"
-      sha256 "8dff526606563ea38c86d645939bf21c560c06804e8b954d7d5146b665ad7023"
-
-      def install
-        bin.install "cherry-go-linux-amd64" => "cherry-go"
-        generate_completions_from_executable(bin/"cherry-go", "completion")
-      end
+      url "https://github.com/theburrowhub/cherry-go/releases/download/v#{version}/cherry-go_#{version}_linux_amd64.tar.gz"
+      sha256 "3cf9982cf07eecfac587ef0d41540d9b84964aa37f1b0ef9bab36802b7ad5f38"
     end
+  end
+
+  def install
+    bin.install "cherry-go"
+    generate_completions_from_executable(bin/"cherry-go", "completion")
   end
 
   test do
