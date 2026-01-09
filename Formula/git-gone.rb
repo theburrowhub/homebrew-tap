fd5040ffee76b5273c64b5cbd0286ff224ee7767
Sergio Tejón <sergio.tejon@gmail.com>
2026-01-09 12:53:02 +0100

    chore(git-gone): update to version 0.8.3 and standardize binary URLs

--- Formula/git-gone.rb
+++ Formula/git-gone.rb
@@ -4,26 +4,26 @@
 class GitGone < Formula
   desc "A simple tool to clean up Git branches"
   homepage "https://github.com/theburrowhub/git-gone"
-  version "0.7.0"
+  version "0.8.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone-macos-arm64.tar.gz"
-      sha256 "817fd37088108f9cb0a0351ca0195f3330ec1623bf2d1137ec7be48816d9881e"
+      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone_#{version}_darwin_arm64.tar.gz"
+      sha256 "e2534e078f940d66bdfd201f8a627b9a184c22f6e1b728e66db877eb680491ba"
 
       def install
-        bin.install "git-gone-macos-arm64" => "git-gone"
+        bin.install "git-gone"
         generate_completions_from_executable(bin/"git-gone", "completion")
       end
     end
 
     on_intel do
-      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone-macos-amd64.tar.gz"
-      sha256 "cff4d90f2d71d2113ea946a723468a8e024d3a6ce3520492d3da892c4ac3098c"
+      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone_#{version}_darwin_amd64.tar.gz"
+      sha256 "bd9714e4723563abe161b5f5edac14845f8304e1ccf4cc198615da211ce23b1a"
 
       def install
-        bin.install "git-gone-macos-amd64" => "git-gone"
+        bin.install "git-gone"
         generate_completions_from_executable(bin/"git-gone", "completion")
       end
     end
@@ -31,21 +31,21 @@ class GitGone < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone-linux-arm64.tar.gz"
-      sha256 "cfedcb066ed1c7f554b42073cf4ba25802318aa39c1f0d17bb235836f9e16b6a"
+      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone_#{version}_linux_arm64.tar.gz"
+      sha256 "e80d7670141e70b0ffbefe71acf733183395509479511e91a5fb5aeb55608120"
 
       def install
-        bin.install "git-gone-linux-arm64" => "git-gone"
+        bin.install "git-gone"
         generate_completions_from_executable(bin/"git-gone", "completion")
       end
     end
 
     on_intel do
-      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone-linux-amd64.tar.gz"
-      sha256 "f3daedeb888870982ac8e7f73288e16980179d5b4f4d76bc00e48152ea735889"
+      url "https://github.com/theburrowhub/git-gone/releases/download/v#{version}/git-gone_#{version}_linux_amd64.tar.gz"
+      sha256 "505365357823ef02c639f76358e792141414dd65e288f06e8fac85c7329b8739"
 
       def install
-        bin.install "git-gone-linux-amd64" => "git-gone"
+        bin.install "git-gone"
         generate_completions_from_executable(bin/"git-gone", "completion")
       end
     end
