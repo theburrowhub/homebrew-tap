e1231d6164ed590a80e34feda45bcc70e5a39be4
jamuriano <jamuriano@freepik.com>
2026-03-05 12:57:43 +0100

    fang: add formula (placeholder SHAs, updated automatically on release)

---
+++ Formula/fang.rb
@@ -0,0 +1,41 @@
+# typed: false
+# frozen_string_literal: true
+
+class Fang < Formula
+  desc "A modern, fast terminal file explorer"
+  homepage "https://theburrowhub.github.io/fang"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/theburrowhub/fang/releases/download/v#{version}/fang_#{version}_aarch64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+
+    on_intel do
+      url "https://github.com/theburrowhub/fang/releases/download/v#{version}/fang_#{version}_x86_64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/theburrowhub/fang/releases/download/v#{version}/fang_#{version}_aarch64-unknown-linux-musl.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+
+    on_intel do
+      url "https://github.com/theburrowhub/fang/releases/download/v#{version}/fang_#{version}_x86_64-unknown-linux-musl.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "fang"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/fang --version")
+  end
+end
